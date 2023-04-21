c6ce51c250934e0a9397572b138f8d18b0749108
falfeya <130572121+falfeya@users.noreply.github.com>
2023-04-21 13:29:49 +0500

    Add files via upload

---
+++ phone/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            phone phone1 = new phone("555-555", "Samsung", 0.5);
+            phone phone2 = new phone("444-444", "Iphone");
+            phone phone3 = new phone();
+
+            Console.WriteLine("Первый телефон:");
+            Console.WriteLine("Номер: " + phone1.getNumber());
+            Console.WriteLine("Модель: " + phone1.model);
+            Console.WriteLine("Вес: " + phone1.weight);
+
+            Console.WriteLine("Второй телефон:");
+            Console.WriteLine("Номер: " + phone2.getNumber());
+            Console.WriteLine("Модель: " + phone2.model);
+            Console.WriteLine("Вес: " + phone2.weight);
+
+            Console.WriteLine("Третий телефон:");
+            Console.WriteLine("Номер: " + phone3.getNumber());
+            Console.WriteLine("Модель: " + phone3.model);
+            Console.WriteLine("Вес: " + phone3.weight);
+
+            phone1.receiveCall("Никита");
+            phone2.receiveCall("Сеня", "111-222-3333");
+            phone3.receiveCall("Артем");
+
+            phone1.sendMessage("111-222-3333", "444-555-6666");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ phone/phone.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phone
+{
+    internal class phone
+    {
+        public string number;
+        public string model;
+        public double weight;
+        public phone(string number, string model, double weight)
+        {
+            this.number = number;
+            this.model = model;
+            this.weight = weight;
+        }
+        public phone(string number, string model)
+        {
+            this.number = number;
+            this.model = model;
+            weight = 0;
+        }
+        public phone()
+        {
+            number = "";
+            model = "";
+            weight = 0;
+        }
+        public void receiveCall(string name)
+        {
+            Console.WriteLine("Звонит " + name);
+        }
+        public void receiveCall(string name, string callNumber)
+        {
+            Console.WriteLine("Звонит " + name + " с номера: " + callNumber);
+        }
+        public string getNumber()
+        {
+            return number;
+        }
+        public void sendMessage(params string[] numbers)
+        {
+            Console.WriteLine("Отправляем сообщение на номера:");
+
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
+    
+
